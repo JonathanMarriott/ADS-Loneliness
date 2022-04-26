6ef990284b851c24302aa5fa9885816617c2654e
Jago <24651555+jagosherred@users.noreply.github.com>
2022-04-26 22:07:29 +0100

    "Update R function to do all variables"

--- Correlation Test(FSM).R
+++ Correlation Test(FSM).R
@@ -1,17 +1,16 @@
 library("ggpubr")
 #Load data - in RStudio working directory should be set to location of file.
+setwd("~/ADS/ADS-Loneliness")
 data <- read.csv("..\\ADS-Loneliness\\master_df.csv")
 
 
-#Omit NAs 
+
 lonely = data$lonely_avg
-FSM    = data$FSM.Eligible..
-both = data.frame(lonely,FSM)
-both = na.omit(both)
-lonely = both$lonely
-FSM = both$FSM
-
-#Test correlation 
-cor.test(FSM,lonely)
-
-
+for (i in 3:ncol(data))
+{
+  print(paste("TESTING: " , toString(colnames(data)[i])))
+  var = data[,i]
+  both = data.frame(lonely,var)
+  both = na.omit(both)
+  print(cor.test(both$lonely,both$var))
+}
